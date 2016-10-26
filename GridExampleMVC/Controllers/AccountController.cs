61d2aeee4be7cc90c684c1cc4bc6fc4adc976a20
Rajesh <cogwave@LAPTOP-GDGLK6H3>
2016-10-26 16:48:08 +0530

    test

--- GridExampleMVC/Controllers/AccountController.cs
+++ GridExampleMVC/Controllers/AccountController.cs
@@ -39,7 +39,6 @@ namespace GridExampleMVC.Controllers
                 _signInManager = value; 
             }
         }
-
         public ApplicationUserManager UserManager
         {
             get
